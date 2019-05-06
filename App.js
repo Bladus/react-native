8092452cae705d6c0a41611cfa55e0d3528c983e
madiev <madiev@corp.sputnik.ru>
2019-05-06 12:36:50 +0300

    added side_menu

--- App.js
+++ App.js
@@ -5,15 +5,34 @@ import {
     TouchableOpacity
 } from 'react-native';
 
+import SideMenu from 'react-native-side-menu';
+
 const url = 'https://picsum.photos/v2/list?limit=10';
+
+const menu_items = [
+    {
+        "title": "menu 1"
+    },
+    {
+        "title": "menu 2"
+    },
+    {
+        "title": "menu 3"
+    },
+    {
+        "title": "menu 4"
+    }
+];
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: []
+            items: [],
+            side_menu_open: false
         }
         this.request = this.request.bind(this);
+        this.onPress = this.onPress.bind(this);
     }
     request() {
         try {
@@ -26,50 +45,72 @@ export default class App extends React.Component {
             return [];
         }
     }
+    onPress() {
+        //Alert.alert('You tapped the button!');
+        this.setState({
+            side_menu_open: true
+        });
+    }
     componentDidMount() {
         this.request();
     }
     render() {
-        let {items} = this.state;
+        let {items, side_menu_open} = this.state;
         return (
             <SafeAreaView style={styles.safe_area}>
                 <View style={styles.wrapper}>
-                    <View style={styles.container_menu} >
-                        <TouchableOpacity
-                            onPress={() => {
-                                Alert.alert('You tapped the button!');
-                            }}
-                        >
-                            <Image
-                                style={styles.menu}
-                                source={require('./assets/menu.png')}
+                    <SideMenu menu={<Menu/>} isOpen={side_menu_open}>
+                        <View style={styles.container_menu} >
+                            <TouchableOpacity
+                                onPress={this.onPress}
+                            >
+                                <Image
+                                    style={styles.menu}
+                                    source={require('./assets/menu.png')}
+                                />
+                            </TouchableOpacity>
+                            <Text style={styles.title} >Заголовок</Text>
+                        </View>
+                        <View style={styles.container} >
+                            <FlatList
+                                data={items}
+                                keyExtractor={(item, index) => index}
+                                numColumns={2}
+                                renderItem={({item}) => (
+                                    <View style={styles.response}>
+                                        <Image 
+                                            style={styles.images}
+                                            source={{uri: item.download_url}}
+                                        />
+                                        <Text style={styles.response_text}>{item.author}</Text>
+                                    </View>
+                                )}
                             />
-                        </TouchableOpacity>
-                        <Text style={styles.title} >Заголовок</Text>
-                    </View>
-                    <View style={styles.container} >
-                        <FlatList
-                            data={items}
-                            keyExtractor={(item, index) => index}
-                            numColumns={2}
-                            renderItem={({item}) => (
-                                <View style={styles.response}>
-                                    <Image 
-                                        style={{width: 150, height: 150}}
-                                        source={{uri: item.download_url}}
-                                    />
-                                    <Text>{item.author}</Text>
-                                </View>
-                            )}
-                        />
-                    </View>
-                    <View style={styles.footer} >
-                        <Text style={{color: '#fff'}} >тестовый проект 2019</Text>
-                    </View>
+                        </View>
+                        <View style={styles.footer} >
+                            <Text style={{color: '#fff'}} >тестовый проект 2019</Text>
+                        </View>
+                    </SideMenu>
                 </View>
             </SafeAreaView>
         )
     };
+}
+
+const Menu = (props) => {
+    return(
+        <View style={styles.sidemenu} >
+            <FlatList
+                data={menu_items}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({item}) => (
+                    <View style={styles.menu_items} >
+                        <Text>{item.title}</Text>
+                    </View>
+                )}
+            />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -98,7 +139,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     title: {
-        marginTop: 5,
+        marginTop: 4.5,
         fontSize: 25,
     },
     menu: {
@@ -106,7 +147,26 @@ const styles = StyleSheet.create({
         height: 40,
         marginRight: 20
     },
+    images: {
+        width: 150,
+        height: 150,
+        borderRadius: 10
+    },
     response: {
         padding: 10,
+        alignItems: 'center'
+    },
+    response_text: {
+        marginTop: 10,
+        color: '#205065',
+        fontSize: 15
+    },
+    sidemenu: {
+        flex: 1,
+        backgroundColor: '#22aade'
+    },
+    menu_items: {
+        padding: 10,
+        fontSize: 20
     }
 });
